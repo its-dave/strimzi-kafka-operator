e80aa7a5a6c4814139ff20bc2039887556fb70a3
Katherine E Speight <KATHERIS@uk.ibm.com>
2020-02-18 10:16:38 +0000

    feat: Switch to using latest tag
    Contributes to mhub/qp-planning#4870
    
    Signed-off-by: Katherine Stanley <katheris@uk.ibm.com>

--- eventstreams-build.sh
+++ eventstreams-build.sh
@@ -31,14 +31,28 @@ echo "Transfer Strimzi artifact to Artifactory..."
 mvn deploy -s ./eventstreams-settings.xml -DskipTests
 
 KAFKA_IMAGE="${destination_registry}/strimzi/kafka-${B_ARCH}:${TAG}"
+KAFKA_IMAGE_LATEST="${destination_registry}/strimzi/kafka-${B_ARCH}:latest-kafka-2.4.0"
+
 OPERATOR_IMAGE="${destination_registry}/strimzi/operator-${B_ARCH}:${TAG}"
+OPERATOR_IMAGE_LATEST="${destination_registry}/strimzi/operator-${B_ARCH}:latest"
 
 echo "Retagging strimzi/kafka:latest to ${KAFKA_IMAGE}..."
 docker tag "strimzi/kafka:latest" "${KAFKA_IMAGE}"
+echo "Retagging strimzi/kafka:latest to ${KAFKA_IMAGE_LATEST}..."
+docker tag "strimzi/kafka:latest" "${KAFKA_IMAGE_LATEST}"
 
 echo "Retagging strimzi/operator:latest to ${OPERATOR_IMAGE}..."
 docker tag "strimzi/operator:latest" "${OPERATOR_IMAGE}"
+echo "Retagging strimzi/operator:latest to ${OPERATOR_IMAGE_LATEST}..."
+docker tag "strimzi/operator:latest" "${OPERATOR_IMAGE_LATEST}"
 
 echo "Pushing images to ${destination_registry}..."
 docker push "${KAFKA_IMAGE}"
 docker push "${OPERATOR_IMAGE}"
+if [[ $TAG == *exp ]]; then
+  echo 'Docker tag ends with "exp" so not pushing latest images'
+else
+  echo 'Docker tag does not end with "exp" so pushing latest images'
+  docker push "${OPERATOR_IMAGE_LATEST}"
+  docker push "${KAFKA_IMAGE_LATEST}"
+fi
