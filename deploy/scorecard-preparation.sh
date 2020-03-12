3de164d55bbd41954b954a4f949e97b6b98e5a7f
Stylianos Gkiokas <Stylianos.Gkiokas@ibm.com>
2020-03-12 11:22:44 +0000

    chore: retrieve scorecard output on Jenkins (#144)
    It seems that some of the instructions from the operator-sdk repo can be
    ingored in order to properly run the scorecard. This commit removes the
    volume and volumeMount update of the ES deployment since it "confuses"
    the scorecard for some reason.
    
    Contributes to: mhub/qp-planning#4773
    
    Signed-off-by: Stelios Gkiokas <Stylianos.Gkiokas@ibm.com>

--- deploy/scorecard-preparation.sh
+++ deploy/scorecard-preparation.sh
@@ -9,23 +9,3 @@ set -e
     name: KUBECONFIG
     value: /scorecard-secret/config
 EOF
-
-	yq w -i -s - operator.yaml <<EOF
-- command: update
-  path: spec.template.spec.containers.(name==eventstreams-cluster-operator).volumeMounts[+]
-  value:
-    name: scorecard-kubeconfig
-    mountPath: /scorecard-secret
-EOF
-
-	yq w -i -s - operator.yaml <<EOF
-- command: update
-  path: spec.template.spec.volumes[+]
-  value:
-    name: scorecard-kubeconfig
-    secret:
-      secretName: scorecard-kubeconfig
-      items:
-      - key: kubeconfig
-        path: config
-EOF
