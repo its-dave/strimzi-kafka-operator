5f244038b71910cc347dd7dbc0e8db97ea6c2b85
Samuel B Hawker <Samuel.Hawker@ibm.com>
2020-03-31 12:57:28 +0100

    feat: Consolidate envars into EVENTSTREAMS (#206)
    Trim envars down to reduce duplication
    Make all envars EVENTSTREAMS specific
    write logic to clone eventstreams envars as strimzi envars
    Use WATCHED_NAMESPACE envar as per cloud pak instructions
    
    Contributes to: mhub/qp-planning#5076
    
    Signed-off-by: Samuel Hawker <samuel.hawker@ibm.com>

--- eventstreams-cluster-operator/scripts/eventstreams_cluster_operator_run.sh
+++ eventstreams-cluster-operator/scripts/eventstreams_cluster_operator_run.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# Clone WATCHED_NAMESPACE as the EVENTSTREAMS_NAMESPACE env var
+export EVENTSTREAMS_NAMESPACE="${WATCHED_NAMESPACE}"
+# Search for all EVENTSTREAMS environmental variables, clone them as STRIMZI env vars and export
+eval "$(env | grep '^EVENTSTREAMS' | sed 's/^EVENTSTREAMS/export STRIMZI/g')"
+
 export JAVA_CLASSPATH=lib/com.ibm.eventstreams.@project.build.finalName@.@project.packaging@:@project.dist.classpath@
 export JAVA_MAIN=com.ibm.eventstreams.Main
-exec ${STRIMZI_HOME}/bin/launch_java.sh
+exec "${STRIMZI_HOME}/bin/launch_java.sh"
