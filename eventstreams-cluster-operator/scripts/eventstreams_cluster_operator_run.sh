82a0869696e77256ecc2f71826c7904c7508f066
Samuel B Hawker <Samuel.Hawker@ibm.com>
2020-04-04 21:23:41 +0100

    chore: Sed command to quote envars (#217)
    for space-delimited lists, export fails
    re-quote strings to avoid these errors
    
    Contributes to: mhub/qp-planning#5076
    
    Signed-off-by: Samuel Hawker <samuel.hawker@ibm.com>

--- eventstreams-cluster-operator/scripts/eventstreams_cluster_operator_run.sh
+++ eventstreams-cluster-operator/scripts/eventstreams_cluster_operator_run.sh
@@ -2,7 +2,8 @@
 # Clone WATCHED_NAMESPACE as the EVENTSTREAMS_NAMESPACE env var
 export EVENTSTREAMS_NAMESPACE="${WATCHED_NAMESPACE}"
 # Search for all EVENTSTREAMS environmental variables, clone them as STRIMZI env vars and export
-eval "$(env | grep '^EVENTSTREAMS' | sed 's/^EVENTSTREAMS/export STRIMZI/g')"
+# Quote envars to avoid export errors
+eval "$(env | grep '^EVENTSTREAMS' | sed 's/^EVENTSTREAMS/export STRIMZI/g' | sed 's/=/="/1; s/$/"/1')"
 
 export JAVA_CLASSPATH=lib/com.ibm.eventstreams.@project.build.finalName@.@project.packaging@:@project.dist.classpath@
 export JAVA_MAIN=com.ibm.eventstreams.Main
