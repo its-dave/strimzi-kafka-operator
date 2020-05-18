ec08b9f5596a998e31ff5e19720851ed8a7b1319
H. E. Elsom <Harvey.Elsom@uk.ibm.com>
2020-05-18 10:42:43 +0100

    fix: remove need for sudo in build (#372)
    * fix: remove need for sudo in build
    
    Contributes to: mhub/qp-planning#5489
    
    Signed-off-by: Harvey Elsom <harvey.elsom@uk.ibm.com>
    Signed-off-by: Dave Lane <davilane@uk.ibm.com>

--- deploy/convertYaml2Json.sh
+++ deploy/convertYaml2Json.sh
@@ -1,38 +1,28 @@
 #!/usr/bin/env bash
 
-DEFAULT_OS=darwin
-if [ -z ${B_OS} || ${B_OS} == "macos" ]; then 
-    read -p "os in use (default: darwin): " B_OS
-    B_OS=${B_OS:-${DEFAULT_OS}}
+if [[ ${B_OS} == "macos" ]]; then 
+    B_OS=darwin
 fi
-os=${B_OS}
+org="bronze1man"
+repo="yaml2json"
+tag="latest"
+artifact="yaml2json_${B_OS}_amd64"
+binary_name="yaml2json"
 
-download_artifact() {
-    org=$1
-    repo=$2
-    tag=$3
-    artifact=$4
-    binary_name=$5
-    list_asset_url="https://api.github.com/repos/${org}/${repo}/releases/${tag}"
-    echo "list asset url: ${list_asset_url}"
-    echo "artifact: ${artifact}"
-    curl "${list_asset_url}" | jq ".assets[] | select(.name==\"${artifact}\") | .url"
-    asset_url=$(curl "${list_asset_url}" | jq ".assets[] | select(.name==\"${artifact}\") | .url" | sed 's/\"//g')
-    echo "downloading asset: ${asset_url}"
-    echo "curl -LJ -H 'Accept: application/octet-stream' "${asset_url}" -o ${binary_name}"
-    curl -LJ -H 'Accept: application/octet-stream' "${asset_url}" -o ${binary_name}
-    sudo mkdir -p /tmp/bin
-    sudo mv ${binary_name} /tmp/bin/${binary_name}
-    sudo chmod +x /tmp/bin/${binary_name}
-}
+list_asset_url="https://api.github.com/repos/${org}/${repo}/releases/${tag}"
+echo "list asset url: ${list_asset_url}"
+echo "artifact: ${artifact}"
+curl "${list_asset_url}" | jq ".assets[] | select(.name==\"${artifact}\") | .url"
+asset_url=$(curl "${list_asset_url}" | jq ".assets[] | select(.name==\"${artifact}\") | .url" | sed 's/\"//g')
+echo "downloading asset: ${asset_url}"
+echo "curl -LJ -H 'Accept: application/octet-stream' \"${asset_url}\" -o ./${binary_name}"
+curl -LJ -H 'Accept: application/octet-stream' "${asset_url}" -o "./${binary_name}"
+chmod +x "./${binary_name}"
 
-download_artifact "bronze1man" "yaml2json" "latest" "yaml2json_${os}_amd64" "yaml2json"
-
-
-for yamlfile in olm-catalog/ibm-eventstreams/*/*.yaml
+for yamlfile in olm-catalog/ibm-eventstreams/2.0.0/*.yaml
 do
-    jsonfile=$(sed -e "s|.yaml|.json|g" <<< ${yamlfile})
-    echo $jsonfile
-    /tmp/bin/yaml2json < ${yamlfile} > ${jsonfile}
-    rm -f ${yamlfile}
+    ./${binary_name} < "${yamlfile}" > "${yamlfile//.yaml/.json}"
+    rm -f "${yamlfile}"
 done
+
+rm -f ./${binary_name}
